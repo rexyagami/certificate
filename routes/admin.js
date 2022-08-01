b9456c8383ac95ed209b35942fed6c0f8b411569
InvincibleNobita <invincible.manish1417@gmail.com>
2022-08-01 11:03:06 +0530

    added /upload-csv route for uploading csv

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const uploads = require("../config/s3");
+const multer = require("multer");
+const csv = require("csvtojson");
+const Image = require("../models/image")
 
 // Create Admin
 router.get("/", (req, res) => {
     res.render("upload");  
 });
 
-router.post("/", uploads.any(), (req, res) => {
+router.post("/", uploads.single('file'), (req, res) => {
     console.log(req.body)
-    res.redirect("/admin/mailer");
-    // if (!req.files) {
-    //     res.status(400).send("nofile");
-    //     console.log(req.file);
-    //     return;
-    //   }
-    
-    //   for (var i = 0; i < req.files.length; i++) {
-    //     // Image.create({
-    //     //   image: req.files[i].location,
-    //     // });
-    
-    //     console.log(`Success!\n Image uploaded to ${req.files[i].location}`);
-    
-    //     if (i == req.files.length - 1) {
-    //       res.redirect("/mailer");
-    //     }
-    //   }
+    console.log(req.file);
+    if (!req.files) {
+            res.status(400).send("nofile");
+            console.log(req.file);
+            return;
+          }
+    var imagePath = req.file.path
+    Image.create({
+        image: imagePath
+    });
+
+    console.log(`Success!\n Image uploaded to ${imagePath}`);
+
+            // if (i == req.files.length - 1) {
+            //   res.redirect("/mailer");
+            // }
+          
+    // console.log(req.files[1],req.files[1].path)
+    res.send("done")
     // CreateHackathon.updateOne(
     //     { hackathonName: req.body.hackName },
     //     {
@@ -42,6 +46,50 @@ router.post("/", uploads.any(), (req, res) => {
         // res.send({ status: "success", code: "done" });
     // });
 });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(path.join(__dirname, "../public/upload/"));
+    cb(null, path.join(__dirname, "../public/upload/"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.post("upload-csv", upload.single('csv'), (req, res) => {
+  csv()
+    .fromFile(req.file.path)
+    .then((jsonObj) => {
+      console.log(jsonObj);
+      for (var x = 0; x < jsonObj.length; x++) {
+        jsonObj[x].hackathon = [];
+        const i = jsonObj[x]["Total"];
+        for (j = 1; j <= i; j++) {
+          jsonObj[x].hackathon.push(jsonObj[x][`hackathon ${j}`]);
+        }
+        console.log(jsonObj[x]);
+        User.create(jsonObj[x], (err, data) => {
+          if (err) {
+            console.log(err);
+            throw err;
+          } else console.log(data);
+        });
+        // delete newUser[name]
+        // Innovator.create(jsonObj[x], (err, data) => {
+        //   if (err) {
+        //     console.log(err);
+        //     throw err;
+        //   } else console.log(data);
+        // });
+      }
+      // res.redirect("/");
+    //   res.redirect("back");
+    });
+    res.redirect("/admin/mailer");
+})
+
 router.get("/mailer", (req, res) => {
     res.render("mailer");  
 });
