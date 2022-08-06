1fa239cd8eb27b7cf6b663c26338c23be8247e46
InvincibleNobita <invincible.manish1417@gmail.com>
2022-08-06 13:15:44 +0530

    added api for updating user role

---
+++ controllers/api.js
@@ -0,0 +1,22 @@
+const Innovator = require("../models/innovator");
+
+module.exports.ChangeInnovatorRole = (req, res) => {
+    Innovator.updateOne(
+        {
+            "email": req.body.email
+        },{
+            $set: {
+                "role": req.body.role
+            }
+        }
+    ).then((doc) => {
+        console.log(doc)
+        res.send("done")
+        // res.redirect("/admin/users")
+    }).catch((err) => {
+        console.log(err)
+        res.redirect("/admin/users")
+    })
+    
+
+}
