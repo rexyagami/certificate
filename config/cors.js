9a81862154c0773f735ee253858ffcc92c01fdba
Rex Yagami <40285992+rexyagami@users.noreply.github.com>
2022-08-08 12:24:57 +0530

    modified-CORS-re wrote -1

--- config/cors.js
+++ config/cors.js
@@ -3,11 +3,10 @@ module.exports = (req, res, next) => {
 
   const index = process.env.WHITELIST_DOMAINS.split(",").indexOf(origin);
 
-  // if (index > -1) {
-  //     res.header("Access-Control-Allow-Origin", origin);
-  // }
+  if (index > -1) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
 
-  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "GET,PUT,POST,PATCH,DELETE,OPTIONS"
